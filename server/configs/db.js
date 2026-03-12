ca64483b2cb207ce01ecd42e3314f3d83db07c7f
ShagunGupta-tech <shagungupta147@gmail.com>
2026-03-12 14:28:01 +0530

    connection error

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,9 +1,17 @@
 import mongoose, { connect } from "mongoose";
 
-const  connectDB = async()=>{
+const connectDB = async () => {
     try {
-        mongoose.connection.on('connected', ()=> console.log('MongoDB connected successfully'))
-        await mongoose.connect(`${process.env.MONGODB_URL}/social`)
+        mongoose.connection.on('connected', () => console.log('MongoDB connected successfully'));
+
+        const rawUrl = process.env.MONGODB_URL || '';
+        const cleanedUrl = rawUrl.replace(/\/+$/, ''); // remove trailing slashes
+
+        // If MONGODB_URL already includes a database path, do not append /social again.
+        const hasDatabase = cleanedUrl.match(/\/[^\/\?]+(\?|$)/);
+        const connectionString = hasDatabase ? cleanedUrl : `${cleanedUrl}/social`;
+
+        await mongoose.connect(connectionString);
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
     }
